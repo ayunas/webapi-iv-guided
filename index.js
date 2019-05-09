b407b15528f0ec6a9256cd0d11d2befc441d0b17
ayunas <ayunas@gmail.com>
2019-05-09 12:28:12 -0500

    set up process.env variables

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 // envSetup.config();
 require('dotenv').config();
 
-
 const server = require('./api/server.js');
+
+// server.get('/', (req,res) => {
+//   const shoutouts = 
+//   res.status(200).json({message : process.env.MOTD})
+// })
 
 server.listen(process.env.PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${process.env.PORT}  ***\n`);
