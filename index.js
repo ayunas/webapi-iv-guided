6226d58a081a03c25172a98591a552cd5a4a760b
ayunas <ayunas@gmail.com>
2019-05-09 11:51:44 -0500

    PORT variable in server.listen()

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
+server.listen(process.env.PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${process.env.PORT}  ***\n`);
 });
