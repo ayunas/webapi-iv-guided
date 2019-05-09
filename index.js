7d2e6b18401457b93523bb9f9e9febdd1b02c0dc
ayunas <ayunas@gmail.com>
2019-05-09 11:48:38 -0500

    set up server with nodemon for heroku

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// const envSetup = require('dotenv');
+// envSetup.config();
+require('dotenv').config();
+
+
 const server = require('./api/server.js');
 
 server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  console.log(`\n*** Server Running on http://localhost:${process.env.PORT}  ***\n`);
 });
